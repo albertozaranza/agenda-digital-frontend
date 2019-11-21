7efac7236c2e618f40a4c80f7a59aacd3cad1530
Alberto Zaranza <albertozaranza@outlook.com>
2019-11-20 22:28:11 -0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 
-// import { Container } from './styles';
+import Routes from '~/routes';
+import history from '~/services/history';
 
 export default function App() {
-  return <h1>Hello</h1>;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
