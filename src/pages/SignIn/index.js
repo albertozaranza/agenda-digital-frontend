e663b3b2331708182e9fe61a4cbac9b2bc8bd3d4
Alberto Zaranza <albertozaranza@outlook.com>
2019-11-21 14:37:36 -0300

    stylization of signin and signup routes

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { MdBook } from 'react-icons/md';
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <div className="logo">
+        <MdBook />
+        Agenda Digital
+      </div>
+      <form>
+        <input type="email" placeholder="Digite seu email" />
+        <input type="password" placeholder="Digite sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="register">Criar conta</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { MdBook } from 'react-icons/md';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <div className="logo">
+        <MdBook />
+        Agenda Digital
+      </div>
+      <form>
+        <input placeholder="Nome completo" />
+        <input type="email" placeholder="Digite seu email" />
+        <input type="password" placeholder="Digite sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
